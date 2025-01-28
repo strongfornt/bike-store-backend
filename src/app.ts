1a10a15cda59d23d0a44ce280c7c4aa57c75a1af
storngfornt <au0038173@gmail.com>
2025-01-28 11:24:42 +0600

    cors policy updated

--- src/app.ts
+++ src/app.ts
@@ -10,7 +10,7 @@ const app: Application = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser())
-app.use(cors({origin: ['http://localhost:5173']}));
+app.use(cors({ origin: true }));
 
 
 app.get('/', async (req: Request, res: Response) => {
